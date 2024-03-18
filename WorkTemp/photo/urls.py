83736bc13123b90ea63dd5577bc9fd3ab3774eee
scorp <scorp@scorp>
2024-03-18 13:36:01 +0800

    refactoring all

--- WorkTemp/photo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth import views
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('login/', lambda re: print(re), name='login'),
-]
--- WorkTemp/substation/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SubstationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'substation'
