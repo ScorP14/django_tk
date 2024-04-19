e8328728e9f3dbc58196d05cb26866c848c5c8be
scorp <scorp@scorp>
2024-04-19 20:39:24 +0800

    add icon

--- config/settings.py
+++ config/settings.py
@@ -106,9 +106,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = (BASE_DIR / 'static')
+STATIC_URL = '/static/'
 MEDIA_ROOT = (BASE_DIR / 'media')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+
+]
 MEDIA_URL = '/media/'
 
 # Default primary key field type
