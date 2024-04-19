9707c7380ad241803e0064dd00f6f9e1eb632b08
scorp <scorp@scorp>
2024-04-19 20:14:46 +0800

    paginator for substation

--- templatetags/paginator_tags.py
+++ templatetags/paginator_tags.py
@@ -1,27 +1,19 @@
 from django import template
-from django.core.paginator import Paginator
 
 register = template.Library()
 
 
 @register.simple_tag
-def get_proper_elided_page_range(pag, number, on_each_side=3, on_ends=2):
-    # paginator = Paginator(pag.object_list, pag.per_page)
-    paginator = Paginator(range(1000), pag.per_page)
-    q = []
-    for i in paginator.get_elided_page_range(number=50, on_each_side=on_each_side, on_ends=on_ends):
-        q.append(i)
-
-    middle_index = len(q) // 2
-    for ind, ch in enumerate(q):
-        if ch == paginator.ELLIPSIS:
-            if ind < middle_index:
-                q[ind] = q[middle_index] // 2
+def get_proper_elided_page_range(pag, number, on_each_side=2, on_ends=1):
+    if pag.num_pages <= 9:
+        return range(1, pag.num_pages+1)
+    paginator_nums_list = list(pag.get_elided_page_range(number, on_each_side=on_each_side, on_ends=on_ends))
+    middle_index = len(paginator_nums_list) // 2
+    for index, char in enumerate(paginator_nums_list):
+        if char == pag.ELLIPSIS:
+            if index < middle_index:
+                paginator_nums_list[index] = paginator_nums_list[middle_index] // 2 + 1
             else:
-                q[ind] = (q[-1] + q[middle_index]) // 2
-    print(q)
-    return paginator.get_elided_page_range(
-        number=number,
-        on_each_side=on_each_side,
-        on_ends=on_ends
-    )
+                paginator_nums_list[index] = (paginator_nums_list[-1] + paginator_nums_list[middle_index]) // 2 + 1
+    return paginator_nums_list
+
